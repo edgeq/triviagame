61876e4ef680ce877cf74d2959605f7ff1fe8402
Edge Quintanilla <edgarq86@gmail.com>
2017-09-29 16:18:07 -0500

    As good as it's gonna get

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,47 @@
-//array for trivia quesstions
+//objects for trivia questions
 //TO DO: pick actual questions
 
-var triviaObject1 = {
+var triviaQuestions = [
+	{
 	question: "Where is clark kent from?",
 	answers: ["Metropolis", "Krypton", "Gotham", "Smallville"],  
 	correct: "Krypton"
-}
-
-var triviaObject2 = {
+},
+	{
 	question: "Where is aquaman from?",
 	answers: ["The water", "the sea", "space", "thermal vents"],  
 	correct: "the sea"
 }
+]
+var currentQuestion = 0
 
-
-var roundTimer = 30;
-var intervalId
+var round1Timer = 30;
+var round2Timer = 30
+var round3Timer = 30;
+var intervalId;
 var postRoundTimer;
-var score = 0
-var wins = 0
+var score = 0;
+var wins = 0;
 
 //onclick for StartGame
-	$(".start").click(startGame);
+$(".start").click(startGame);
 //  The runTimer function sets an interval
 //  that runs the following decrement function once a second.
 function runTimer() {
+  round1timer = 30;
   intervalId = setInterval(timer, 500);
 }
 
 //timer function(s)
 function timer() {
  //  Decrease number by one.
-  roundTimer--;
-  console.log(roundTimer);
-  $("#countdown").html("<p><h2>Time left to answer: " + roundTimer);
+  round1Timer--;
+  console.log(round1Timer);
+  $("#countdown").html("<h2>Time left to answer: " + round1Timer);
   
-  if (roundTimer === 0) {
-    //  ...run the stop function.
-    newQuestion();
-    //  Alert the user that time is up.
-    
+  if (round1Timer === 0) {
     clearInterval(intervalId);
+    newQuestion();
   }
 }
 
@@ -49,7 +50,6 @@ function startGame() {
 	console.log("game started")
 	runTimer();
 	var trivQuestion = $("<div>");
-	trivQuestion.addClass("trivia");
 	$("#game-area").append(trivQuestion);
 	newQuestion();
 
@@ -59,38 +59,58 @@ function startGame() {
 
 //new question after each round
 function newQuestion() {
+	 console.log("new question")
 	 $("#game-area").empty();
-	var trivDiv = $("<div>");
-	trivDiv.addClass("triv-ans");
-	$("#game-area").prepend(trivDiv);
 
 	var screenQuestion = $("#trivia").add("<h2>")
-		screenQuestion.text(triviaObject1.question);
+		screenQuestion.text(triviaQuestions[currentQuestion].question);
 		$("#game-area").append(screenQuestion);
 		answerChoices();
+
+	currentQuestion++
 		 
 }
 
 function answerChoices() {
-	for (var i = 0; i < triviaObject1.answers.length; i++) {
+	for (var i = 0; i < triviaQuestions[currentQuestion].answers.length; i++) {
 		var screenAnswer = $("#triv-ans").add("<h2>").attr("class", "bg-info")
-			screenAnswer.text(triviaObject1.answers[i]);
+			screenAnswer.text(triviaQuestions[currentQuestion].answers[i]);
 			$("#game-area").append(screenAnswer);
 		
-	}
-	
+	}	
 		
 }
-
 
 //reward screen if correct
 function rewardScreen() {
 
 }
 
+function resetTimer() {
+	if (round1Timer <= 0){
+		round1Timer = 30;
+		clearInterval(intervalId);
+		
+		stagResponse();
+}
+
 //stagnant response function
 function stagResponse() {
-	// roundTimer = 
+	 console.log("second question")
+	 $("#game-area").empty();
+	var trivDiv = $("<div>");
+	// trivDiv.addClass("triv-ans");
+	// $("#game-area").prepend(trivDiv);
+
+	var screenQuestion = $("#trivia").add("<h2>")
+		screenQuestion.text(triviaObject2.question);
+		$("#game-area").append(screenQuestion);
+		answerChoices();
+	
+
+		
+
+	}
 
 }
 //wrong response function
